8c82c2e6a953d15278d1385ece118501e797fead
zaburo-ch <kaizaburo_chubachi@shino.ecei.tohoku.ac.jp>
2019-01-28 12:00:27 +0900

    Add plots

--- rsfalearning/plot_results.py
+++ rsfalearning/plot_results.py
@@ -5,7 +5,8 @@ import numpy as np
 
 
 def get_results(filename):
-    results = pd.read_csv(filename, header=None).iloc[:50000]
+    results = pd.read_csv(filename, header=None)
+    assert len(results) == 100000
     header = [
         'i', 'target_state', 'dsfa_state', 'dsfa_mq', 'dsfa_eq', 'dsfa_eq_cached',
         'dsfa_ce_guard', 'dsfa_ce_state', 'dsfa_det_ce', 'dsfa_comp_ce',
@@ -42,7 +43,7 @@ int_interval_results = get_results('int_interval_results')
 sns.lineplot(x='dsfa_state', y='value', hue='variable',
     data=int_interval_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_eq', 'rsfa_eq']))
 plt.xlim(0, 200)
-plt.ylim(0, 550)
+plt.ylim(0, 750)
 plt.xlabel('Size of minimal DFSA')
 plt.ylabel('Number of asked EQs')
 plt.savefig('int_interval_results_eq.pdf')
@@ -58,12 +59,12 @@ plt.savefig('int_interval_results_mq.pdf')
 plt.close()
 
 
-new_equality_results = get_results('new_condition3_equality_results_2_backup')
+new_equality_results = get_results('new_condition3_equality_results_2')
 equality_results['new_condition_3_rsfa_eq'] = new_equality_results['rsfa_eq']
 equality_results['new_condition_3_rsfa_mq'] = new_equality_results['rsfa_mq']
 
 sns.lineplot(x='dsfa_state', y='value', hue='variable',
-    data=equality_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_eq', 'rsfa_eq', 'new_condition_3_rsfa_eq']))
+    data=equality_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_eq', 'new_condition_3_rsfa_eq']))
 plt.xlim(0, 200)
 plt.ylim(0, 550)
 plt.xlabel('Size of minimal DFSA')
@@ -72,7 +73,16 @@ plt.savefig('new_condition3_equality_results_eq.pdf')
 plt.close()
 
 sns.lineplot(x='dsfa_state', y='value', hue='variable',
-    data=equality_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_mq', 'rsfa_mq', 'new_condition_3_rsfa_mq']))
+    data=equality_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_eq', 'rsfa_eq', 'new_condition_3_rsfa_eq']))
+plt.xlim(0, 200)
+plt.ylim(0, 550)
+plt.xlabel('Size of minimal DFSA')
+plt.ylabel('Number of asked EQs')
+plt.savefig('new_condition3_equality_results_with_old_eq.pdf')
+plt.close()
+
+sns.lineplot(x='dsfa_state', y='value', hue='variable',
+    data=equality_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_mq', 'new_condition_3_rsfa_mq']))
 plt.xlabel('Size of minimal DFSA')
 plt.ylabel('Number of asked MQs')
 plt.xlim(0, 200)
@@ -80,18 +90,45 @@ plt.ylim(0, 12500)
 plt.savefig('new_condition3_equality_results_mq.pdf')
 plt.close()
 
+sns.lineplot(x='dsfa_state', y='value', hue='variable',
+    data=equality_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_mq', 'rsfa_mq', 'new_condition_3_rsfa_mq']))
+plt.xlabel('Size of minimal DFSA')
+plt.ylabel('Number of asked MQs')
+plt.xlim(0, 200)
+plt.ylim(0, 12500)
+plt.savefig('new_condition3_equality_results_with_old_mq.pdf')
+plt.close()
 
-new_int_interval_results = get_results('new_condition3_int_interval_results_backup')
+
+new_int_interval_results = get_results('new_condition3_int_interval_results')
 int_interval_results['new_condition_3_rsfa_eq'] = new_int_interval_results['rsfa_eq']
 int_interval_results['new_condition_3_rsfa_mq'] = new_int_interval_results['rsfa_mq']
 
 sns.lineplot(x='dsfa_state', y='value', hue='variable',
-    data=int_interval_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_eq', 'rsfa_eq', 'new_condition_3_rsfa_eq']))
+    data=int_interval_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_eq', 'new_condition_3_rsfa_eq']))
 plt.xlim(0, 200)
-plt.ylim(0, 550)
+plt.ylim(0, 750)
 plt.xlabel('Size of minimal DFSA')
 plt.ylabel('Number of asked EQs')
 plt.savefig('new_condition3_int_interval_results_eq.pdf')
+plt.close()
+
+sns.lineplot(x='dsfa_state', y='value', hue='variable',
+    data=int_interval_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_eq', 'rsfa_eq', 'new_condition_3_rsfa_eq']))
+plt.xlim(0, 200)
+plt.ylim(0, 750)
+plt.xlabel('Size of minimal DFSA')
+plt.ylabel('Number of asked EQs')
+plt.savefig('new_condition3_int_interval_results_with_old_eq.pdf')
+plt.close()
+
+sns.lineplot(x='dsfa_state', y='value', hue='variable',
+    data=int_interval_results.melt(id_vars=['dsfa_state'], value_vars=['dsfa_mq', 'new_condition_3_rsfa_mq']))
+plt.xlabel('Size of minimal DFSA')
+plt.ylabel('Number of asked MQs')
+plt.xlim(0, 200)
+plt.ylim(0, 220000)
+plt.savefig('new_condition3_int_interval_results_mq.pdf')
 plt.close()
 
 sns.lineplot(x='dsfa_state', y='value', hue='variable',
@@ -99,6 +136,6 @@ sns.lineplot(x='dsfa_state', y='value', hue='variable',
 plt.xlabel('Size of minimal DFSA')
 plt.ylabel('Number of asked MQs')
 plt.xlim(0, 200)
-plt.ylim(0, 12500)
-plt.savefig('new_condition3_int_interval_results_mq.pdf')
+plt.ylim(0, 220000)
+plt.savefig('new_condition3_int_interval_results_with_old_mq.pdf')
 plt.close()
